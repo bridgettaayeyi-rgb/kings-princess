3526638da7184f81fe9252c1ecb8e1232fd0d66d
bridgettaayeyi-rgb <bridgettaayeyi@gmail.com>
2025-12-18 22:32:34 +0000

    Fix stylesheet path in testimonies.php

--- view/testimonies.php
+++ view/testimonies.php
@@ -4,7 +4,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Testimonies</title>
-<link rel="stylesheet" href="../designs/testimony.css">
+<link rel="stylesheet" href="designs/testimony.css">
 </head>
 
 <body>
