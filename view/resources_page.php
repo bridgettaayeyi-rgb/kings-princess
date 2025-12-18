e4a4cd6c0eed67b7f3c8f0d4b29d03abd12634c2
bridgettaayeyi-rgb <bridgettaayeyi@gmail.com>
2025-12-18 22:31:58 +0000

    Fix stylesheet link path in resources_page.php

--- view/resources_page.php
+++ view/resources_page.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Resources</title>
-    <link rel="stylesheet" href="../designs/resources.css">
+    <link rel="stylesheet" href="designs/resources.css">
 </head>
 <body>
 
