6cfd36ab8f6a34b670b0dfa555b59f34e73142a8
bridgettaayeyi-rgb <bridgettaayeyi@gmail.com>
2025-12-18 22:27:56 +0000

    Fix stylesheet link path in edit_devotion.php

--- view/edit_devotion.php
+++ view/edit_devotion.php
@@ -4,7 +4,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Edit Devotion</title>
-<link rel="stylesheet" href="../designs/prayer.css">
+<link rel="stylesheet" href="designs/prayer.css">
 </head>
 <body>
 
