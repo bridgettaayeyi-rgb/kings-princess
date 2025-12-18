61740b3aba321bb33a8890170dc7e72db7d29ae3
bridgettaayeyi-rgb <bridgettaayeyi@gmail.com>
2025-12-18 22:31:37 +0000

    Fix stylesheet link path in profile_page.php

--- view/profile_page.php
+++ view/profile_page.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>My Profile</title>
-    <link rel="stylesheet" href="../designs/profile.css">
+    <link rel="stylesheet" href="designs/profile.css">
 </head>
 <body>
 
