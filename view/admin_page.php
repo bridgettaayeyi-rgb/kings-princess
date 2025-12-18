965dea0b4b5fef1208f9068418f7688afe2f2fd9
bridgettaayeyi-rgb <bridgettaayeyi@gmail.com>
2025-12-18 22:26:39 +0000

    Fix stylesheet link path in admin_page.php

--- view/admin_page.php
+++ view/admin_page.php
@@ -8,7 +8,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Admin Dashboard</title>
-<link rel="stylesheet" href="../designs/admin.css">
+<link rel="stylesheet" href="designs/admin.css">
 <style>
     nav {
     display: flex;
